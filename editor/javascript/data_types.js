27c09fdcc6d3d00b3d38967a8656e107b2146c9f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-13 18:48:40 -0400

    Revert "Added "javascript", "html", and "css" subfolders of "editor" folder"
    This reverts commit 8ff33240d1086207d4a6fee724a6448c2fb12586.

---
+++ editor/javascript/data_types.js
@@ -0,0 +1,31 @@
+const dataTypes = [
+      {
+            "dataType": "array",
+            "name": "Array",
+            "color": "#0F0"
+      },
+      {
+            "dataType": "string",
+            "name": "String",
+            "color": "#F00"
+      },
+      {
+            "dataType": "tensor",
+            "name": "Tensor"
+      },
+      {
+            "dataType": "number",
+            "name": "Number",
+            "color": "#00F"
+      },
+      {
+            "dataType": "boolean",
+            "name": "Boolean"
+      },
+      {
+            "dataType": "scalar",
+            "name": "Scalar"
+      }
+];
+
+console.log("Data types loaded. (data_types.js)")
