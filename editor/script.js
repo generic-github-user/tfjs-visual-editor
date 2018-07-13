10fc970d87c9dc5382542bc90d2ac67adde01ffb
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-13 07:57:25 -0400

    Added nodeList and more styling

--- editor/script.js
+++ editor/script.js
@@ -5,18 +5,44 @@ const nodes = {
       "tensors": {
             "tensor": {
                   // We could get the title from the HTML menu, but that would be somewhat messy
-                  "title": "Tensor"
+                  "title": "Tensor",
+                  "information": "https://js.tensorflow.org/api/latest/index.html#tensor"
             },
             "scalar": {
-                  "title": "Scalar"
+                  "title": "Scalar",
+                  "information": "https://js.tensorflow.org/api/latest/index.html#scalar"
             }
       }
 }
 
+const nodeList = [
+
+];
+
+var newNode;
 var nodeHTML;
 const add = function (node) {
-      nodeHTML = "<div class='node'>";
+      newNode = {
+            "title": node.title,
+            "name": "",
+            "description": "",
+            "display": {
+                  "position": {
+                        "x": 200,
+                        "y": 200
+                  },
+                  "dimensions": {
+                        "width": 400,
+                        "height": 200
+                  }
+            }
+      };
+
+
+      nodeHTML = "<div class='node' style='width:" + newNode.display.dimensions.width + "; height:" + newNode.display.dimensions.height + ";'>";
       nodeHTML += "<h4>" + node.title + "</h4>";
       nodeHTML += "</div>";
       document.querySelector("#editor").innerHTML += nodeHTML;
+
+      nodeList.push(newNode);
 }
