3617da1537d412258105092833bfb754859dae67
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-07-14 10:59:42 -0400

    Added update() function and node setup saving with web browser localStorage (#30)

--- editor/javascript/script.js
+++ editor/javascript/script.js
@@ -1,6 +1,56 @@
 var nodeList = [
 
 ];
+
+function update() {
+      document.querySelector("#editor").innerHTML = "";
+
+      nodeList.forEach(
+            function (node) {
+                  if (!document.getElementById(node.id)) {
+                        const main = node.elements.main;
+                        node.node.data.inputs.forEach(
+                              (input) => {
+                                    color = dataTypes.find(x => x.dataType == input.dataTypes[0]).color;
+                                    main.innerHTML += "<div class='node-data' style='background-color:" + color + ";'></div>";
+                              }
+                        );
+                        node.node.data.outputs.forEach(
+                              (output) => {
+                                    color = dataTypes.find(x => x.dataType == output.dataTypes[0]).color;
+                                    main.innerHTML += "<div class='node-data' style='background-color:" + color + ";'></div>";
+                              }
+                        );
+
+                        const style = "style='\
+                              width: " + node.display.dimensions.width + "px; \
+                              height: " + node.display.dimensions.height + "px; \
+                              left: " + node.display.position.x + "px; \
+                              top: " + node.display.position.y + "px;\
+                        ";
+                        document.querySelector("#editor").innerHTML += "\
+                              <div class='node' " + style + " id='" + node.id + "'>\
+                                    <h4>" + node.node.title + "</h4>\
+                                    <a href='" + node.node.info + "' target='_blank' class='mdl-card__menu' id='" + "info-" + node.id + "'>\
+                                          <button class='mdl-button mdl-js-button mdl-button--icon mdl-button--colored mdl-js-ripple-effect'>\
+                                                <i class='material-icons'>info</i>\
+                                          </button>\
+                                    </a>\
+                                    <div class='mdl-tooltip' for='" + "info-" + node.id + "'>\
+                                          Documentation\
+                                    </div>\
+                              </div>\
+                        ";
+                  }
+            }
+      );
+
+      console.log("Saving editor data to browser localStorage . . .");
+      localStorage.setItem("tfjs-visual-editor", JSON.stringify(nodeList));
+      console.log("Editor data saved:");
+      console.log(nodeList);
+}
+
 function UUID() {
       function s4() {
             return Math.floor((1 + Math.random()) * 0x10000)
@@ -16,9 +66,7 @@ function clone(object) {
 const savedData = localStorage.getItem("tfjs-visual-editor");
 if (savedData) {
       nodeList = JSON.parse(savedData);
-      nodeList.forEach(
-            (node) => addNode(node)
-      );
+      update();
 }
 
 var newNode;
@@ -61,48 +109,8 @@ function getNodeInfo(nodeType) {
 
 var color;
 function addNode(node) {
-      document.querySelector("#editor").innerHTML = "";
-
-      const main = node.elements.main;
-      node.node.data.inputs.forEach(
-            (input) => {
-                  color = dataTypes.find(x => x.dataType == input.dataTypes[0]).color;
-                  main.innerHTML += "<div class='node-data' style='background-color:" + color + ";'></div>";
-            }
-      );
-      node.node.data.outputs.forEach(
-            (output) => {
-                  color = dataTypes.find(x => x.dataType == output.dataTypes[0]).color;
-                  main.innerHTML += "<div class='node-data' style='background-color:" + color + ";'></div>";
-            }
-      );
-
-      const style = "style='\
-            width: " + node.display.dimensions.width + "px; \
-            height: " + node.display.dimensions.height + "px; \
-            left: " + node.display.position.x + "px; \
-            top: " + node.display.position.y + "px;\
-      ";
-      document.querySelector("#editor").innerHTML += "\
-            <div class='node' " + style + " id=" + node.id + ">\
-                  <h4>node.node.title</h4>\
-                  <a href='" + node.node.info + "' target='_blank'>\
-                        <button class='mdl-button mdl-js-button mdl-button--icon mdl-button--colored mdl-js-ripple-effect mdl-card__menu' id='" + "info-" + node.id + "'>\
-                              <i class='material-icons'>info</i>\
-                        </button>\
-                  </a>\
-                  <div class='mdl-tooltip' for='" + "info-" + node.id + "'>\
-                        Documentation\
-                  </div>\
-            </div>\
-      ";
-
       nodeList.push(node);
-
-      console.log("Saving editor data to browser localStorage . . .");
-      localStorage.setItem("tfjs-visual-editor", JSON.stringify(nodeList));
-      console.log("Editor data saved:");
-      console.log(nodeList);
+      update();
 }
 
 console.log("Main editor script loaded. (script.js)");
